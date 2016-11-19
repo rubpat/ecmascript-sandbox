90f69b3929cf304c18eb4f0e1dd61245e4b93f57
rubpat <ruben.patvakanyan@yahoo.com>
2016-11-19 13:28:03 +0400

    Update function_parameters.js

--- functions/function_parameters.js
+++ functions/function_parameters.js
@@ -1,24 +1,24 @@
 /**
- * those problems cover functions
- * in problem1 the issue is with the declaration of function exmpl
- * in problem2 the issue is with the invalid use of function exmpl
+ * Topic: Functions
+ * Instructions: in problem1 the issue is with the declaration of function exmpl
+ *               in problem2 the issue is with the invalid use of function exmpl
  *
  * Relevant resource:
- * - http://www.w3schools.com/js/js_functions.asp
+ * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
  */
 
 function problem1(){
   function exmpl(var x, var y)
   {
   	return x+y;
-  	}
+  }
 }
 
 function problem2(){
    function exmpl(x, y, z)
   {
   	return x*y*z;
-  	}
+  }
 
   	var a = 5;
   	var b = 4;
@@ -27,25 +27,14 @@ function problem2(){
 
 /**
  *
- * Solution - below is a base64 blob of the solution.(problem1)
+ * Solution - below is a base64 blob of the solution.
  *
 --begin--
-<
-  ZnVuY3Rpb24gcHJvYmxlbTEoKXsKICBmdW5jdGlvbiBleG1wbCh4LCB5KQogIHsKICByZXR1cm4g
-eCt5OwogIH0KfQo=
 
->
---end--
- *
- * * Solution - below is a base64 blob of the solution.(problem2)
- *
---begin--
-<
-  ZnVuY3Rpb24gcHJvYmxlbTIoKXsKICAgZnVuY3Rpb24gZXhtcGwoeCwgeSwgeiwpCiAgewogIHJl
+  ZnVuY3Rpb24gcHJvYmxlbTEoKXsKICBmdW5jdGlvbiBleG1wbCh4LCB5KQogIHsKICByZXR1cm4g
+eCt5OwogIH0KfQo= ZnVuY3Rpb24gcHJvYmxlbTIoKXsKICAgZnVuY3Rpb24gZXhtcGwoeCwgeSwgeiwpCiAgewogIHJl
 dHVybiB4KnkqejsKICB9CiAgCiAgdmFyIGEgPSA1OwogIHZhciBiID0gNDsKICBjb25zb2xlLmxv
 ZyhleG1wbChhLCBiKSk7Cn0K
 
-
->
 --end--
  */
